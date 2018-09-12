617bf51e7275e6da45a9096603e92cac5fa8ea28
Brandon <b.charest019@gmail.com>
2018-09-12 13:15:25 -0400

    Added opcodes.txt and init

--- Chip-8/chip8.h
+++ Chip-8/chip8.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <cstdint>
+#include <array>
+using std::array;
+
+#include <string>
+using std::string;
+
 class chip8
 {
 public:
@@ -9,30 +15,32 @@ public:
 	bool drawFlag;
 	void emulateCycle();
 	void debugRender();
-	bool loadApplication(char* const fileName);
+	bool loadApplication(string const fileName);
 
-	// Screen has a total of 2048 pixels (64 x 32)
-	uint8_t gfx[64 * 32];
+	// Screen has a total of 2048 pixels (64 x 32)	
+	array<uint8_t, 64 * 32> gfx;
 
-	// Chip 8 uses hex based keypad (0x0-0xF)
-	uint8_t key[16];
+	// Chip 8 uses hex based keypad (0x0-0xF)	
+	array<uint8_t, 16> key;
 
 private:
 	uint16_t m_program_counter;
+	// Chip 8 has 35 opcodes
 	uint16_t m_opcode;
 	uint16_t m_index_register;
-	uint16_t m_stack_pointer;
-	uint16_t m_stack[16];
+	uint16_t m_stack_pointer;	
+	array<uint16_t, 16> m_stack;
 
 	// Chip 8 has 15 8-bit general purpose registers.
-	// the 16th register is used for the carry flag.
-	uint8_t m_V[16];
+	// the 16th register is used for the carry flag.	
+	array<uint8_t, 16> m_V;
 
 	// 4k memory
-	uint8_t m_memory[4096];
+	array<uint8_t, 4096> m_memory;	
 	uint8_t m_delay_timer;
 	uint8_t m_sound_timer;
 
 	void init();
 };
+
 
